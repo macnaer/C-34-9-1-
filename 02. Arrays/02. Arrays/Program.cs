7fed9e5be62c34d6ae0e68c6d97098f8f495d12a
master <master@rivne.itstep.org>
2020-02-25 14:00:50 +0200

    List<Person>

--- 02. Arrays/02. Arrays/Program.cs
+++ 02. Arrays/02. Arrays/Program.cs
@@ -19,23 +19,27 @@ namespace _02.Arrays
             ////}
 
             ////Array arr = new int[5];
-            //Array arr = Array.CreateInstance(typeof(int), 5);  // не зручно
+            ///*Array arr = Array.CreateInstance(typeof(int), 5);*/  // не зручно
             ////arr[0] = 10; // Error
-            //arr.SetValue(22, 0);
-            //Console.WriteLine(arr.GetValue(0));
+            ////arr.SetValue(22, 0);
+            ////arr.SetValue(18, 1);
+
+            ////Console.WriteLine(arr.GetValue(0));
+            ////Console.WriteLine(arr.GetValue(1));
+
 
             //int[] numbers = { 1,2,3,4,5,6,7,8,9,10};
-            //int index = Array.BinarySearch(numbers, 8);    // Перше входження у відсортованому масиві
-            //Console.WriteLine("Index of 8 = {0}", index);
+            ////int index = Array.BinarySearch(numbers, 8);    // Перше входження у відсортованому масиві
+            ////Console.WriteLine("Index of 8 = {0}", index);
 
             //int[] numbers2 = new int[10];
             //Array.Copy(numbers, numbers2, numbers.Length);
 
-            //foreach(int value in numbers2)
-            //{
-            //    Console.WriteLine(value);
-            //}
-            //Console.WriteLine("========================================>");
+            ////foreach (int value in numbers2)
+            ////{
+            ////    Console.WriteLine(value);
+            ////}
+            ////Console.WriteLine("========================================>");
 
             //int[] numbers3 = new int[10];
             //numbers2.CopyTo(numbers3, 0);
@@ -57,18 +61,49 @@ namespace _02.Arrays
             //}
             //Console.WriteLine("#########################################>");
             //#endregion
-            #region
-            var list = new List<int>() { 34, 435, 346, 67, 68 };
-            list.Add(11);
 
-            foreach(var value in list)
-            {
-                Console.WriteLine(value);
-            }
-            Console.WriteLine("========================================>");
-            list.Insert(2, 100500);
-            Console.WriteLine(list[2]);
-            #endregion
+
+            //#region
+            //var list = new List<int>();
+            //list.Add(11);
+            //list.Add(14);
+            //list.Add(56);
+            //list.Add(1);
+            //list.Add(56);
+            //list.Sort();
+            //list.Insert(2, 100500);
+            //list.RemoveAt(0);
+            ////list.Clear();
+            //list.Remove(56);
+
+            //foreach (var value in list)
+            //{
+            //    Console.WriteLine(value);
+            //}
+            //Console.WriteLine("========================================>");
+            ////Console.WriteLine(list[2]);
+            //Console.WriteLine(list.Sum());
+            ////Console.WriteLine(list.Average());
+            //#endregion
+
+            //#region
+            //var list = new List<Person>();
+            //list.Add(new Person { name = "Bill", surname = "Gates", age = 57 });
+            //list.Add(new Person { name = "Stive", surname = "Balmor", age = 59 });
+            //list.Add(new Person { name = "Ronald", surname = "MacDonald", age = 61 });
+
+            //foreach(var item in list)
+            //{
+            //    Console.WriteLine("Name: {0}\nSurname: {1}\nAge: {2}", item.name, item.surname, item.age);
+            //    Console.WriteLine("==============================================>>>>");
+            //}
+            //#endregion
         }
+    }
+    class Person
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public byte age { get; set; }
     }
 }
