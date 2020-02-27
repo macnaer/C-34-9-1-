e2c33868a9e2a339b5538934fce94abee1fe7af6
macnaer <trofimchuk.an@gmail.com>
2020-02-27 08:04:51 +0200

    Start with string

---
+++ 04. String/04. String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "somegoodword";
+            bool cW = name.Contains("w");
+            Console.WriteLine(cW);
+
+            string word = string.Concat("w", "o", "r", "d");
+            Console.WriteLine(word);
+
+            int num = 100;
+            string snum = num.ToString();
+            Console.WriteLine(snum);
+        }
+    }
+}
