88ef2d79c6de451a12688071bf07131001efb91f
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-03-02 13:06:35 +0200

    OOP Sample

---
+++ 05. OOP Intro/05. OOP Intro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// public 
+// private 
+// protected
+// internal
+
+namespace _05.OOP_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Swordman kozak = new Swordman();
+            kozak.Hit(10);
+            Console.WriteLine(kozak.Health);
+            kozak.Hit(10, 30);
+            Console.WriteLine(kozak.Health);
+            //kozak.Karma = 11;
+            //Console.WriteLine(kozak.Karma);
+
+
+            //Console.WriteLine("<<<========================================>>>");
+            int avg = kozak.AvgDamage(new int[] { 30, 22, 32, 12,70 });
+            Console.WriteLine("Avg damage = {0}", avg);
+            //Console.WriteLine("<<<---------------------------------------->>>");
+
+            //int newAvg = kozak.NewAvgDamage(24, 20, 52);
+            int newAvg = kozak.NewAvgDamage(24, 20, 52, 10 , 100);
+            Console.WriteLine("New damage = {0}", newAvg);
+
+        }
+    }
+}
---
+++ 05. OOP Intro/05. OOP Intro/Swordman.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.OOP_Intro
+{
+    public class Swordman
+    {
+        public int Health { get; private set; } = 100;// Get set
+
+        private readonly string skill = "Fight";
+
+        int karma;
+
+        public int Karma
+        {
+            get
+            {
+                return karma;
+            }
+            set
+            {
+                if (value == 10)
+                {
+                    karma = value;
+                }
+                else
+                {
+                    Console.WriteLine("Bad karma!");
+                }
+                
+            }
+        }
+
+
+        internal void Hit(int damage)
+        {
+            if (damage > Health)
+            {
+                damage = Health;
+            }
+            Health -= damage;
+
+        }
+
+        public void Hit(int damage, int fatality)
+        {
+            if (damage > Health)
+            {
+                damage = Health;
+            }
+            Health -= damage;
+
+            if (fatality > Health)
+            {
+                fatality = Health;
+            }
+            Health -= fatality;
+        }
+
+        public int AvgDamage(int[] damage)
+        {
+            int sum = 0;
+
+            foreach(int item in damage)
+            {
+                sum += item;
+            }
+
+            return sum / damage.Length;
+        }
+
+        public int NewAvgDamage(params int[] damage)
+        {
+            int sum = 0;
+
+            foreach (int item in damage)
+            {
+                sum += item;
+            }
+
+            return sum / damage.Length;
+        }
+    }
+}
