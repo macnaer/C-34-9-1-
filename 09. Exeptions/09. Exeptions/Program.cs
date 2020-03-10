09fb3a331444f3c709fd3c0c26abb1c68aa6f7c4
macnaer <trofimchuk.an@gmail.com>
2020-03-10 08:11:41 +0200

    Exeptions

---
+++ 09. Exeptions/09. Exeptions/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Exeptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter number: ");
+            //string result = Console.ReadLine();
+
+            //int number = 0;
+
+            //try
+            //{
+            //    number = int.Parse(result);
+            //    Console.WriteLine("Parse result = {0}", number);
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine("Format exeptions: fix it please");
+            //    //Console.WriteLine(ex.ToString());
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Exp => {0}", ex);
+            //}
+
+            //FileStream file = null;
+            //try
+            //{
+            //    file = File.Open("test.txt", FileMode.Open);
+            //}
+            //catch (IOException ex)
+            //{
+            //    Console.WriteLine(ex);
+            //}
+            //finally{
+            //    if (file != null)
+            //    {
+            //        file.Dispose();
+            //    }
+            //}
+
+
+            int age = 0;
+            
+            if (age == 0)
+            {
+                throw new ArgumentNullException("Age can't be null ");
+            }
+            if (age < 0)
+            {
+                throw new ArgumentException("Age can't be less then 0");
+            }
+
+
+        }
+    }
+}
